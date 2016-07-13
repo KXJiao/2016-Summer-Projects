3041e46ba87e36e1c34c4227033aa9b8bc1fb3e3
azhang24 <azhang24@users.noreply.github.com>
2016-07-13 15:29:41 -0400

    added settings button to data page

--- StopSignProject/app/src/main/java/cn/truthvision/stopsignproject/Data.java
+++ StopSignProject/app/src/main/java/cn/truthvision/stopsignproject/Data.java
@@ -12,5 +12,19 @@ public class Data extends AppCompatActivity {
         setContentView(R.layout.activity_data);
 
         Intent i = getIntent();
+        
+        Button settings = (Button) findViewById(R.id.settingsbutton3);
+        settings.setOnClickListener(new View.OnClickListener(){
+           @Override
+            public void onClick(View view){
+               openSettings(view);
+           }
+        });
+    }
+    
+    public void openSettings(View view){
+        Intent intent = new Intent(this, Main.class);
+        startActivity(intent);
+        //finish();
     }
 }
