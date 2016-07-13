5f59546f7e5f60f720e84f00f21e1a6e7aa89776
azhang24 <azhang24@users.noreply.github.com>
2016-07-13 15:46:52 -0400

    imageview added

--- StopSignProject/app/src/main/java/cn/truthvision/stopsignproject/Home.java
+++ StopSignProject/app/src/main/java/cn/truthvision/stopsignproject/Home.java
@@ -12,7 +12,8 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
+        
+        ImageView stopsign = (ImageView) findViewById(R.id.stopsignimage);
         Intent i = getIntent();
 
         Button s = (Button) findViewById(R.id.button);
