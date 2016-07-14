deea90648f7a5c38f3268a89b3bbd503cb89f70f
Kevin Jiao <kxjiao@gmail.com>
2016-07-14 16:00:27 -0400

    Video edits

--- StopSignProject/app/src/main/java/cn/truthvision/stopsignproject/Video.java
+++ StopSignProject/app/src/main/java/cn/truthvision/stopsignproject/Video.java
@@ -1,7 +1,9 @@
 package cn.truthvision.stopsignproject;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,8 +11,22 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import cn.truthvision.stopsignlib.VideoManager;
+
 public class Video extends AppCompatActivity {
-    private static final int REQUEST_VIDEO_CAPTURE = 1;
+    private static final int CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE = 200;
+    private Uri fileUri;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,28 +35,79 @@ public class Video extends AppCompatActivity {
         //setSupportActionBar(toolbar);
 
         Intent i = getIntent();
-        
+
+
+
+
+        /*Note: I will be editing the video portion so please do not touch the code between these comments
+        **************************************************************************************************
+        **************************************************************************************************
+        **************************************************************************************************
+        **************************************************************************************************
+        **************************************************************************************************
+        **************************************************************************************************
+        **************************************************************************************************
+        **************************************************************************************************
+        **************************************************************************************************
+        **************************************************************************************************
+         */
+
+
+
+
+        //create new Intent
+        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+
+        int MEDIA_TYPE_VIDEO = 2;
+
+        fileUri = VideoManager.getOutputMediaFileUri(MEDIA_TYPE_VIDEO);  // create a file to save the video
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);  // set the image file name
+
+        intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1); // set the video image quality to high
+
+        // start the Video Capture Intent
+        startActivityForResult(intent, CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE);
+
+
+
+        /*
+        **************************************************************************************************
+        **************************************************************************************************
+        **************************************************************************************************
+        **************************************************************************************************
+        **************************************************************************************************
+        **************************************************************************************************
+        **************************************************************************************************
+        **************************************************************************************************
+        **************************************************************************************************
+        **************************************************************************************************
+         */
+
+
+
         Button settings = (Button) findViewById(R.id.settingsbutton2);
-        settings.setOnClickListener(new View.OnClickListener(){
+        settings.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 onSettings(view);
             }
         });
         Button data = (Button) findViewById(R.id.data);
-        data.setOnClickListener(new View.OnClickListener(){
+        data.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 onData(view);
             }
         });
         Button start_recording = (Button) findViewById(R.id.start_recording);
-        start_recording.setOnClickListener(new View.OnClickListener(){
+        start_recording.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 onStartRecording(view);
             }
         });
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,22 +116,66 @@ public class Video extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
-    
-    public void onSettings(View view){
+
+    public void onSettings(View view) {
         Intent intent = new Intent(this, Main.class);
         startActivity(intent);
         finish();
     }
 
-    public void onStartRecording(View view){
+    public void onStartRecording(View view) {
 
     }
 
-    public void onData(View view){
+
+
+    public void onData(View view) {
         Intent intent = new Intent(this, Data.class);
         startActivity(intent);
         finish();
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Video Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://cn.truthvision.stopsignproject/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Video Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://cn.truthvision.stopsignproject/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
