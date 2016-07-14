d94b332c6c1ecd2e1d4e945ef3329f7a5f9caef5
Kevin Jiao <kxjiao@gmail.com>
2016-07-14 11:22:18 -0400

    Initial branch
    ui change: theme, buttons, general fix

--- StopSignProject/app/src/main/java/cn/truthvision/stopsignproject/Video.java
+++ StopSignProject/app/src/main/java/cn/truthvision/stopsignproject/Video.java
@@ -15,8 +15,8 @@ public class Video extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         Intent i = getIntent();
         
