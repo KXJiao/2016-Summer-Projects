c6463dcf3fa1df67d26ac1529aa5d85d694e32a8
azhang24 <azhang24@users.noreply.github.com>
2016-07-20 14:56:46 -0400

    added start to home

--- StopSignProject/app/src/main/java/cn/truthvision/stopsignproject/Home.java
+++ StopSignProject/app/src/main/java/cn/truthvision/stopsignproject/Home.java
@@ -23,10 +23,26 @@ public class Home extends AppCompatActivity {
                 openSettings(v);
             }
         });
+        
+        Button start = (Button) findViewById(R.id.button);
+        start.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                start(v);
+            }
+        });
     }
 
     private void openSettings(View v) {
         Intent intent = new Intent(this, Main.class);
+        startActivity(intent);
+    }
+    
+    private void start(View v){
+        Intent intent = new Intent(this, Recording.class); /* think that the Video class is not necessary because if the user presses "START" it directs to the Recording page and then
+        after the video is saved the app transitions to the "Data" page. For now it would be fine to keep the "Video" page but ultimately we may not need it. On basis of this, a "Video" button
+        in the "Settings" page is unnecessary as well.*/
+
+        /*Option 2: Keep the Video page but allow that page to serve the same functionality as the current data page (i.e. storing videos). The Data page can then be used for statistics, graphs, etc. */
         startActivity(intent);
     }
 }
