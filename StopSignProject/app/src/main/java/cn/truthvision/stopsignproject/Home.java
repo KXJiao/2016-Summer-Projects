6a08192a62b737d4fd6d7dc5ddd1dc9a07427461
Kevin Jiao <kxjiao@gmail.com>
2016-07-28 09:56:36 -0400

    reverted styles
    back to black

--- StopSignProject/app/src/main/java/cn/truthvision/stopsignproject/Home.java
+++ StopSignProject/app/src/main/java/cn/truthvision/stopsignproject/Home.java
@@ -1,5 +1,6 @@
 package cn.truthvision.stopsignproject;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class Home extends AppCompatActivity {
+public class Home extends Activity {
 
 
     @Override
