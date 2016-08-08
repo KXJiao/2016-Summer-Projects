d65583d0603af5ce9daeadebf56615bd8fff01ac
Kevin Jiao <kxjiao@gmail.com>
2016-08-08 16:59:50 -0400

    LiveStream base class
    deleted part of it

---
+++ StopSignProject/stopsignlib/src/main/java/cn/truthvision/stopsignlib/LiveStream.java
@@ -0,0 +1,15 @@
+package cn.truthvision.stopsignlib;
+
+import android.os.ParcelFileDescriptor;
+
+import java.net.Socket;
+
+/**
+ * Created by TV_Laptop_01 on 8/8/2016.
+ */
+public class LiveStream {
+//    String host = "1234";
+//    Socket socket = new Socket(host, 1);
+//    ParcelFileDescriptor pfd = ParcelFileDescriptor.fromSocket(socket);
+
+}
